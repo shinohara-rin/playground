a9569c5fd34c9447348670bad1e08338d0132786
Rin <shinohara-rin@users.noreply.github.com>
2024-03-17 23:46:26 +0800

    wip

--- transcript.py
+++ transcript.py
@@ -1,5 +1,20 @@
+import json
 from youtube_transcript_api import YouTubeTranscriptApi
+from os import environ
+from dotenv import load_dotenv
 
-t = YouTubeTranscriptApi.get_transcript("5C_HPTJg5ek")
+load_dotenv(override=True)
 
-print(t)
+video_list = [
+    "5C_HPTJg5ek"
+]
+
+for video_id in video_list:
+    t = YouTubeTranscriptApi.get_transcript(video_id, proxies={
+                                            'http': environ['http_proxy']} if environ.get('http_proxy') else None)
+    with open(f'./transcripts/{video_id}.json', 'w+') as f:
+        json.dump(t, f, ensure_ascii=False, indent=4)
+
+    plaintext = " ".join([i['text'] for i in t])
+    with open(f'./transcripts/{video_id}.txt', 'w+') as f:
+        f.write(plaintext)
